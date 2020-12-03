9742580791d64cfc4e0f3f23a10b5699602b1eb3
wchesley <wrchesley1@buffs.wtamu.edu>
2020-12-03 10:05:21 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ACM CCS Extractor
+
+## Description
+One off script I wrote to fixture up ACM Computing Classification System items as django-taggit fixtures
+
+## Useage 
+Clone repo  
+cd into repo directory and run  
+`python acm_ccs_extractor.py`
---
+++ acm_ccs_extractor.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import sys
+json_file = open("acm_css_fixture.json", "w+")
+html = open("ref.html", "r")
+try:
+    soup = BeautifulSoup(html, "html.parser")
+    print("read the file")
+    id = 1
+    openbrace = "{"
+    closebrace = "}"
+    json_file.write("[")
+    ccs = soup.find_all(href="javascript:void(0)")
+    for item in ccs:
+        print(f"CCS:{item.text} ID:{id}\n")
+        json_file.write(
+            f"\n{openbrace}\n\t\"model\":\"taggit.Tag\",\n\t\"pk\":{id},\n\t\"fields\":{openbrace}\n\t\t\"name\":\"{item.text}\",\n\t\t\"slug\":\"{item.text}\"\n\t{closebrace}\n{closebrace},")
+        id += 1
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
+json_file.write("]")
+json_file.close()
