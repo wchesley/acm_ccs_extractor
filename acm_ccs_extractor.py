8b396d805b081fc6bbc4e8a69d9661d89cddd5b2
Walker Chesley <chesley.walker@gmail.com>
2020-12-03 22:31:44 -0600

    added logic to remove duplicate entries
    there were 186 duplicates total

--- acm_ccs_extractor.py
+++ acm_ccs_extractor.py
@@ -9,12 +9,15 @@ try:
     openbrace = "{"
     closebrace = "}"
     json_file.write("[")
+    no_dups = []
     ccs = soup.find_all(href="javascript:void(0)")
     for item in ccs:
-        print(f"CCS:{item.text} ID:{id}\n")
-        json_file.write(
-            f"\n{openbrace}\n\t\"model\":\"taggit.Tag\",\n\t\"pk\":{id},\n\t\"fields\":{openbrace}\n\t\t\"name\":\"{item.text}\",\n\t\t\"slug\":\"{item.text}\"\n\t{closebrace}\n{closebrace},")
-        id += 1
+        if item.text not in no_dups:
+            no_dups.append(item.text)
+            print(f"CCS:{item.text} ID:{id}\n")
+            json_file.write(
+                f"\n{openbrace}\n\t\"model\":\"taggit.Tag\",\n\t\"pk\":{id},\n\t\"fields\":{openbrace}\n\t\t\"name\":\"{item.text}\",\n\t\t\"slug\":\"{item.text}\"\n\t{closebrace}\n{closebrace},")
+            id += 1
 except:
     print("Unexpected error:", sys.exc_info()[0])
     raise
